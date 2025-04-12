055da754844bd98524653586b0013dab8de6ac40
Mohammed Sadik <codeprogrammer111@gmail.com>
2025-04-12 09:51:20 +0300

    feat: restructure dashboard and login layouts, add admin members page, and remove unused login component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,23 +4,27 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
 
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col">
-      <SidebarProvider>
-        <AppSidebar />
-        <main className="w-full h-full">
-          <div className="flex space-x-6 items-center ">
-            <SidebarTrigger style={{marginLeft: "3px"}}/>
-            <Navbar name="Henok" />
-          </div>
-          {children}
-        </main>
-      </SidebarProvider>
-    </div>
+    <html>
+      <body>
+        <div className="flex flex-col">
+          <SidebarProvider>
+            <AppSidebar />
+            <main className="w-full h-full">
+              <div className="flex space-x-6 items-center ">
+                <SidebarTrigger style={{ marginLeft: "3px" }} />
+                <Navbar name="Henok" />
+              </div>
+              {children}
+            </main>
+          </SidebarProvider>
+        </div>
+      </body>
+    </html>
   );
 }
